3a15fcdcfbec6abc1e3073a69012b9f613745eaa
Pepe Becker <mail@pepebecker.com>
2018-02-19 21:51:05 +0100

    Update modules

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const api = require('../index')
 describe('Convert', () => {
 	it('should convert characters to Pinyin', () => {
 		return api.convert('我的猫喜欢喝牛奶').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
+			data.text.should.equal('wǒ de māo xǐhuan hē niúnǎi')
 		})
 	})
 })
@@ -14,7 +14,7 @@ describe('Split', () => {
 	it('should split the text into the correct words', () => {
 		return api.split('wodemaoxihuanheniunai').then((data) => {
 			const list = ['wo', 'de', 'mao', 'xi', 'huan', 'he', 'niu', 'nai']
-			data.should.deepEqual(list)
+			data.data.should.deepEqual(list)
 		})
 	})
 })
